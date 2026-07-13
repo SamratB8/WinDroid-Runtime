5272d31c3442f2096812cadaaf26c355e33b615d
Samrat Banerjee <106741933+SamratB8@users.noreply.github.com>
2026-07-13 21:19:28 +0530

    Add Sentry error monitoring

--- src/WinDroid.Studio/App.xaml.cs
+++ src/WinDroid.Studio/App.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml;
+using Sentry;
 
 namespace WinDroid.Studio;
 
@@ -7,16 +8,37 @@ namespace WinDroid.Studio;
 /// </summary>
 public partial class App : Application
 {
+    private readonly IDisposable _sentry;
     private Window? _window;
 
     public App()
     {
+        _sentry = SentrySdk.Init(options =>
+        {
+            options.Dsn = Environment.GetEnvironmentVariable("SENTRY_DSN");
+
+#if DEBUG
+            options.Debug = true;
+#else
+            options.Debug = false;
+#endif
+
+            options.AutoSessionTracking = true;
+        });
+
         InitializeComponent();
     }
 
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
         _window = new MainWindow();
+        _window.Closed += OnMainWindowClosed;
         _window.Activate();
+    }
+
+    private async void OnMainWindowClosed(object sender, WindowEventArgs args)
+    {
+        await SentrySdk.FlushAsync(TimeSpan.FromSeconds(2));
+        _sentry.Dispose();
     }
 }
